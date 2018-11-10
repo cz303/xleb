74873cbab7d63ccf9acfcaf56626adf1e1f3a9f6
mamyebal13373 <44707746+mamyebal13373@users.noreply.github.com>
2018-11-10 14:49:31 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,18 +13,6 @@ class User:
     def __init__(self, name):
         self.name = name
         
-var express = require('express');
-var app     = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
         
 @bot.message_handler(commands=['start'])  #команда#
 def handle_start(message):
